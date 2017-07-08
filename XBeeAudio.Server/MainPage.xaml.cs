d4ba4768d055d07f68d80f5a1b2963b2f92516e4
jefffhaynes <feedyurhed@gmail.com>
2017-07-08 11:22:08 -0400

    initial commit

---
+++ XBeeAudio.Server/MainPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using XBee;
+using XBee.Universal;
+
+namespace XBeeAudio
+{
+    public sealed partial class MainPage : Page
+    {
+        private XBeeController _controller;
+        private MediaCapture _capture;
+        private XBeeNode _remoteNode;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            await FindXBeeAsync();
+            await InitializeAudioAsync();
+        }
+
+        private async Task FindXBeeAsync()
+        {
+            var controllers = await XBeeController.FindControllersAsync(9600);
+
+            if (controllers.Count == 0)
+            {
+                StatusText.Text = "No controllers.";
+                return;
+            }
+
+            StatusText.Text = "Found controller \n";
+
+            _controller = controllers.First();
+
+            _controller.NodeDiscovered += (sender, args) =>
+            {
+                _remoteNode = args.Node;
+                StatusText.Text += $"Discovered remote node: {args.Name}\n";
+            };
+
+            await _controller.DiscoverNetworkAsync(TimeSpan.FromSeconds(5));
+        }
+
+        private async Task InitializeAudioAsync()
+        {
+            try
+            {
+                MediaCaptureInitializationSettings settings = new MediaCaptureInitializationSettings
+                {
+                    StreamingCaptureMode = StreamingCaptureMode.Audio
+                };
+
+                _capture = new MediaCapture();
+
+                _capture.RecordLimitationExceeded += sender => throw new Exception("Record Limitation Exceeded ");
+
+                _capture.Failed += (sender, errorEventArgs) => throw new Exception(string.Format("Code: {0}. {1}", errorEventArgs.Code, errorEventArgs.Message));
+
+                await _capture.InitializeAsync(settings);
+
+                StatusText.Text += "Audio initialized\n";
+            }
+            catch (Exception e)
+            {
+                StatusText.Text += $"Audio failed: {e.Message}";
+            }
+        }
+
+        private async void PushToTalkButton_OnPressed(object sender, RoutedEventArgs e)
+        {
+            await _capture.StartRecordToStreamAsync(MediaEncodingProfile.CreateMp3(AudioEncodingQuality.Low), 
+                new XBeeStreamWrapper(_remoteNode.GetSerialStream()));
+        }
+    }
+}
---
+++ XBeeAudio.Server/XBeeStreamWrapper.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Storage.Streams;
+
+namespace XBeeAudio
+{
+    public class XBeeStreamWrapper : IRandomAccessStream
+    {
+        private readonly Stream _stream;
+        private readonly IOutputStream _outputStream;
+        private readonly IInputStream _inputStream;
+
+        public XBeeStreamWrapper(Stream stream)
+        {
+            _stream = stream;
+            _outputStream = _stream.AsOutputStream();
+            _inputStream = _stream.AsInputStream();
+        }
+
+        public void Dispose()
+        {
+            _stream?.Dispose();
+        }
+
+        public IAsyncOperationWithProgress<IBuffer, uint> ReadAsync(IBuffer buffer, uint count, InputStreamOptions options)
+        {
+            return _inputStream.ReadAsync(buffer, count, options);
+        }
+
+        public IAsyncOperationWithProgress<uint, uint> WriteAsync(IBuffer buffer)
+        {
+            Debug.WriteLine(buffer.Length);
+
+            return AsyncInfo.Run<uint, uint>(async (token, progress) =>
+            {
+                var block = buffer.ToArray();
+                var memoryStream = new MemoryStream(block);
+
+                byte[] b = new byte[100];
+                while (memoryStream.Read(b, 0, b.Length) > 0)
+                {
+                    await _outputStream.WriteAsync(b.AsBuffer());
+                }
+                
+                progress.Report(buffer.Length);
+                return buffer.Length;
+            });
+        }
+
+        public IAsyncOperation<bool> FlushAsync()
+        {
+            return AsyncInfo.Run(_ => Task.FromResult(true));
+        }
+
+        public IInputStream GetInputStreamAt(ulong position)
+        {
+            throw new NotSupportedException();
+        }
+
+        public IOutputStream GetOutputStreamAt(ulong position)
+        {
+            throw new NotSupportedException();
+        }
+
+        public void Seek(ulong position)
+        {
+        }
+
+        public IRandomAccessStream CloneStream()
+        {
+            throw new NotSupportedException();
+        }
+
+        public bool CanRead => _stream.CanRead;
+        public bool CanWrite => _stream.CanWrite;
+        public ulong Position => 0;
+
+        public ulong Size
+        {
+            get { return 0; }
+            set {  }
+        }
+    }
+}
